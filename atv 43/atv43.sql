eda5b295f4d173544bec8d69205cccccdf12c55b
Gislaine Amancio <gimcjf@gmail.com>
2021-11-11 06:53:31 -0300

    Continuação atividade 43 SQL

--- atv 43/atv43.sql
+++ atv 43/atv43.sql
@@ -15,3 +15,15 @@ INSERT INTO public.produto
 VALUES('Computador', 'LG All in One Quad Core 4GB ', 3336, 6069, 2);
 
 select id, nome, preco from produto where nome like 'C%';
+
+-- Liste os dados das colunas Id, nome e preço da tabela Produto 
+--e os dados id, nome e descrição da tabela Categoria para as linhas que possuem um preço maior que 0.
+SELECT p.Id, p.nome, p.preco, c.id , c.nome , c.descricao 
+FROM produto as p
+INNER JOIN categoria as c
+ON p.categoria_id = c.id 
+
+-- Liste os dados de todas as colunas da tabela Produto 
+--que possuem categoria com Id maior que 2.
+select * from produto where id > 2
+
